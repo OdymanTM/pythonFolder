2888ec1d9a03f00640113055fddba70f0b1cc897
Odyman <palamaris02@gmail.com>
2024-02-26 11:47:05 +0200

    Initial commit

---
+++ lab1.py
@@ -0,0 +1,16 @@
+import requests
+
+url = 'https://instagram.com/'
+
+#url = input()
+
+response = requests.get(url)
+
+print(response.headers)
+
+
+try: print(response.headers['Server'])
+except: print('No server info present')
+
+try: print(response.headers['Set-Cookie'])
+except: print('No cookies present')
