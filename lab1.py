6a7df952dc9074d9a7de9de527281827291dd22d
Odyman <palamaris02@gmail.com>
2024-02-26 12:40:03 +0200

    Input function, scheme check

--- lab1.py
+++ lab1.py
@@ -2,15 +2,19 @@ import requests
 
 url = 'https://instagram.com/'
 
-#url = input()
+url = input('Insert url:')
+if not url.startswith('https://'):
+    url = 'https://' + url
 
-response = requests.get(url)
+with requests.get(url) as response:
+    print('Headers are:' , response.headers)
 
-print(response.headers)
+    try: print(response.headers['Server'])
+    except: print('No server info present')
 
-
-try: print(response.headers['Server'])
-except: print('No server info present')
-
-try: print(response.headers['Set-Cookie'])
-except: print('No cookies present') 
+    cookies = response.headers.get('Set-Cookie')
+    #dioti mporei na einai polla to ekana me auton ton tropo
+    if cookies:
+        print('These are the cookies found: ',cookies)
+    else:
+        print('No cookies present')
